74df4bbbb4f75703937dd9b6bf55b0427bbbd9db
mugisha88 <mugishaemmanuel88@gmail.com>
2022-06-28 17:59:59 +0200

    admin changes

---
+++ resources/views/components/dashboard/navbar.blade.php
@@ -0,0 +1,49 @@
+<nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
+    <div class="scrollbar-inner">
+    
+    <div class="sidenav-header  align-items-center">
+    <a class="navbar-brand" href="javascript:void(0)">
+    <img src="../assets/img/brand/blue.png" class="navbar-brand-img" alt="...">
+    </a>
+    </div>
+    <div class="navbar-inner">
+    
+    <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+    
+    <ul class="navbar-nav">
+    <li class="nav-item">
+    <a class="nav-link active" href="index">
+    <i class="ni ni-tv-2 text-primary"></i>
+    <span class="nav-link-text">Admin</span>
+    </a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="student">
+    <i class="ni ni-planet text-orange"></i>
+    <span class="nav-link-text">Student Report</span>
+    </a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="counselor">
+    <i class="ni ni-pin-3 text-primary"></i>
+    <span class="nav-link-text">Counselor Report</span>
+    </a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="question">
+    <i class="ni ni-single-02 text-yellow"></i>
+    <span class="nav-link-text">Question Report</span>
+    </a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="examples/tables.html">
+    <i class="ni ni-bullet-list-67 text-default"></i>
+    <span class="nav-link-text">Sign Out</span>
+    </a>
+    </li>
+    </ul>
+    
+    </div>
+    </div>
+    </div>
+    </nav>
